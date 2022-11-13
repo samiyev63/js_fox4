74f2c9a081a815874a8626ef3e8a72dc25ae5f42
shaxzod <samiyevshaxzod616@gmail.com>
2022-11-13 06:49:26 +0500

    js masalalar

---
+++ js/main.js
@@ -0,0 +1,97 @@
+// const number = prompt("bu yerga soni kiriting: ");
+// if (number % 2 == 0) {
+//   alert("bu son juft: ");
+// } else {
+//   alert("bu son toq: ");
+// }
+
+// function dtm_ballar(berilgan_bal) {
+//   if (berilgan_bal < 60) {
+//     dtm_text = "Siz oqiy olmaysiz";
+//   } else if (berilgan_bal <= 101) {
+//     dtm_text = " super asosida oqiysiz";
+//   } else if (berilgan_bal <= 170) {
+//     dtm_text = " kontrakt asosida oqiysiz";
+//   } else if (berilgan_bal <= 189) {
+//     ``;
+//     dtm_text = "Siz grand asosida oqiysiz";
+//   } else {
+//     dtm_text = "404";
+//   }
+//   const dtm_xabarEl = document.querySelector("#dtm_xabar");
+//   dtm_xabarEl.textContent = dtm_text;
+// }
+
+// let ball = prompt("Balingizni yozing");
+// ball = dtm_ballar(ball);
+
+// let shaxzod = {
+//     name : "shaxzod",
+//     age : 22,
+
+// }
+
+// const a = prompt("a sonni kiriting");
+// const b = prompt("b sonni kiriting");
+// const c = prompt("c sonni kiriting");
+
+// if (a > b && b > c) {
+//   console.log("berilgan amal togri");
+// } else {
+//   console.log("berilgan amal notogri");
+// }
+
+// function fizzBuzz(number) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (number % 3 === 0) {
+//   } else if (number % 5 === 0) {
+//     console.log("buzz");
+//   } else {
+//     console.log("notogri");
+//   }
+// }
+// const a = prompt("bu yerga son kiriting");
+// console.log(fizzBuzz(a));
+
+// 2-3 masala
+// const number = prompt("bu yerga soni kiriting: ");
+// if (number %2 == 0) {
+//   alert("bu son juft: ");
+// } else {
+//   alert("bu son toq");
+// }
+
+// 4-masala
+
+// const a = prompt("bu yerga soni kiriting: ");
+// const b = prompt("bu yerga soni kiriting: ");
+
+// if (a > 0 && b > 0) {
+//     alert("tog'ri")
+// } else {
+//     alert("notog'ri")
+// }
+
+// 5-masala
+// const a = prompt("a sonni kiriting");
+// const b = prompt("b sonni kiriting");
+// const c = prompt("c sonni kiriting");
+
+// if (a > b && b > c) {
+//   alert("berilgan amal togri");
+// } else {
+//   alert("berilgan amal notogri");
+// }
+
+
+
+
+
+// object
+
+const shaxzod = {
+    name: "shazod2",
+    age: 23,
+}
+console.log{shaxzod.name};
